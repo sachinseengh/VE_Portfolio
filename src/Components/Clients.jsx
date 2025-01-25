6203c8e41c554c8a13b5a6c6b71e3e319daaada9
Sachin kumar Singh <108042077+sachinseengh@users.noreply.github.com>
2025-01-25 20:07:30 +0545

    client review corrected

--- src/Components/Clients.jsx
+++ src/Components/Clients.jsx
@@ -1,73 +1,74 @@
 import React from "react";
 import profile from "../assets/images/ishan.jpeg";
 
-
 const Clients = () => {
   return (
-    <div className="container " id="client">
+    <div className="container" id="client">
       <h1 className="text-center text-4xl text-orange-500 font-devanagari mt-[4rem]">
         Happy Clients
       </h1>
 
       <div className="flex flex-col items-center md:flex-row lg:flex-row justify-center space-y-4 md:space-y-0 md:space-x-4 lg:space-x-6 mt-8">
-   
+      
 
-       {/* Circle 1 */}
-       <div className=" lg:w-1/3 md:w-1/3 w-full flex flex-row md:flex-col lg:flex-col  lg:items-center lg:justify-center justify-start">
-          <div className="w-32 h-32  rounded-full flex items-center justify-center">
+         {/* Circle 1 */}
+         <div className="lg:w-1/3 md:w-1/3 w-full flex flex-col md:flex-col lg:flex-col lg:items-center lg:justify-center justify-center items-center">
+          <div className="w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 rounded-full flex  items-center justify-center">
             <img
               src={profile}
               alt="Client 1"
-              className="w-28 h-28 rounded-full object-cover"
+              className="w-full h-full rounded-full object-cover"
             />
           </div>
-          <div className="md:items-center lg:items-center  px-5">
-          <h1 className="font-devanagari   md:text-xl lg:text-xl text-center">Ishan Sharma</h1>
-          <p className="text-start md:text-center lg:text-center text-sm">
-            An Incredible editor with great knowledge of editing and fonts.Helped me a lot in my youtube journey
-          </p>
+          <div className="md:items-center lg:items-center px-5">
+            <h1 className="font-devanagari md:text-xl lg:text-xl text-center">
+              Ishan Sharma
+            </h1>
+            <p className="text-start md:text-center lg:text-center text-sm border border-gray-600 p-2 rounded-lg shadow-sm shadow-black">
+              An Incredible editor with great knowledge of editing and fonts.
+              Helped me a lot in my YouTube journey.
+            </p>
           </div>
         </div>
-    
-    {/* Circle 1 */}
-    <div className=" lg:w-1/3 md:w-1/3 w-full flex flex-row md:flex-col lg:flex-col  lg:items-center lg:justify-center justify-start">
-          <div className="w-32 h-32  rounded-full flex items-center justify-center">
+
+         {/* Circle 1 */}
+         <div className="lg:w-1/3 md:w-1/3 w-full flex flex-col md:flex-col lg:flex-col lg:items-center lg:justify-center justify-center items-center">
+          <div className="w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 rounded-full flex  items-center justify-center">
             <img
               src={profile}
               alt="Client 1"
-              className="w-28 h-28 rounded-full object-cover"
+              className="w-full h-full rounded-full object-cover"
             />
           </div>
-          <div className=" items-center justify-center px-5">
-          <h1 className="font-devanagari text-center  md:text-xl lg:text-xl">Sachin</h1>
-          <p className="text-start md:text-center lg:text-center text-sm">
-            My name is sachin kumar singh hello hello this is me sachin
-          </p>
+          <div className="md:items-center lg:items-center px-5">
+            <h1 className="font-devanagari md:text-xl lg:text-xl text-center">
+              Ishan Sharma
+            </h1>
+            <p className="text-start md:text-center lg:text-center text-sm border border-gray-600 p-2 rounded-lg shadow-sm shadow-black">
+              An Incredible editor with great knowledge of editing and fonts.
+              Helped me a lot in my YouTube journey.
+            </p>
           </div>
         </div>
-
-
-
-       {/* Circle 1 */}
-       <div className=" lg:w-1/3 md:w-1/3 w-full flex flex-row md:flex-col lg:flex-col  lg:items-center lg:justify-center justify-start">
-          <div className="w-32 h-32  rounded-full flex items-center justify-center">
+         {/* Circle 1 */}
+         <div className="lg:w-1/3 md:w-1/3 w-full flex flex-col md:flex-col lg:flex-col lg:items-center lg:justify-center justify-center items-center">
+          <div className="w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 rounded-full flex  items-center justify-center">
             <img
               src={profile}
               alt="Client 1"
-              className="w-28 h-28 rounded-full object-cover"
+              className="w-full h-full rounded-full object-cover"
             />
           </div>
-          <div className=" items-center justify-center px-5">
-          <h1 className="font-devanagari text-center  md:text-xl lg:text-xl">Sachin</h1>
-          <p className="text-start md:text-center lg:text-center text-sm">
-            My name is sachin kumar singh hello hello this is me sachin
-          </p>
+          <div className="md:items-center lg:items-center px-5">
+            <h1 className="font-devanagari md:text-xl lg:text-xl text-center">
+              Ishan Sharma
+            </h1>
+            <p className="text-start md:text-center lg:text-center text-sm border border-gray-600 p-2 rounded-lg shadow-sm shadow-black">
+              An Incredible editor with great knowledge of editing and fonts.
+              Helped me a lot in my YouTube journey.
+            </p>
           </div>
         </div>
-
-
-
-     
       </div>
     </div>
   );
