5e8a3fcddc9832944f13b1b9d520b8c9fa8c9ff1
Sachin kumar Singh <108042077+sachinseengh@users.noreply.github.com>
2025-02-07 21:19:46 +0545

    configuration for domain name

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
 
-  base: '/VE_Portfolio/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/VE_Portfolio/',
   plugins: [react()],
+
 
   assetsInclude: ['**/*.mp4','**/*.MP4'],
   
